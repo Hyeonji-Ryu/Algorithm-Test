07a2db790023354d4a076f9cc065c4625a7c58c8
Hyeonji-Ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-08-06 17:44:07 +0900

    Create Reverse_String.py

---
+++ solution(easy)/Reverse_String.py
@@ -0,0 +1,28 @@
+# Write a function that reverses a string. The input string is given as an array of characters char[].
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+# You may assume all the characters consist of printable ascii characters.
+
+# Example 1:
+# Input: ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+# Example 2:
+# Input: ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+
+
+
+
+# plan 1
+
+s.reverse()
+
+
+
+# plan 2
+
+copy = s[:]
+for i in range(2, len(s)+1):
+    
+    s.append(copy[-i])
+del s[0:len(copy)-1]
