4000b30efe0431b30ad7db49dbf4ca830a8788d3
Hyeonji-Ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-08-14 02:11:56 +0900

    Update Two_Sum.py

--- solution(easy)/Two_Sum.py
+++ solution(easy)/Two_Sum.py
@@ -9,19 +9,30 @@ class Solution:
         
         answer = []
         
-        while len(answer) == 0:
-            for i in range(0,len(nums)):
+        for i in range(0,len(nums)):
                 
-                    temp = nums[:]
-                    temp.remove(nums[i])
+            temp = nums[:]
+            temp.remove(nums[i])
         
-                    if target - nums[i] in temp:
+            if target - nums[i] in temp:
                         
-                        number = target - nums[i]
+                number = target - nums[i]
                         
-                        if number == nums[i]:
-                            answer = [i,temp.index(number)+1]
-                        else:
-                            answer = [i,nums.index(number)]
-                        break
-        return answer
+                if number == nums[i]:
+                    return [i,temp.index(number)+1]
+                else:
+                    return [i,nums.index(number)]
+
+                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+            temp = dict()
+        
+            for i in range(len(nums)):
+                
+                number = target - nums[i]
+        
+                if number in temp:
+                        return [i,temp[number]]
+                temp[nums[i]] = i
