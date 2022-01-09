a7f3f20d9e47f82e1b0c793a9b019da81952de4e
Jess <42489770+Hyeonji-Ryu@users.noreply.github.com>
2022-01-10 01:52:07 +0900

    Create 1000.rs

---
+++ Rust(baekjoon)/1000.rs
@@ -0,0 +1,18 @@
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// 출력
+// 첫째 줄에 A+B를 출력한다.
+
+
+use std::io;
+
+fn main() {
+    let mut num = String::new();
+    io::stdin().read_line(&mut num).unwrap();
+    let list: Vec<i32> = num.split_whitespace().map(|s| s.parse().expect("")).collect();
+    println!("{:}",list[0]+list[1]);
+}
